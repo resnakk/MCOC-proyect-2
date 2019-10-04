253753845ff5a11c21ca41d6dbd00448863296f7
resnakk <masanchez3@miuandes.cl>
2019-10-04 01:53:14 -0300

    hecho por tomas: condiciones de choque

--- entrega_2.py
+++ entrega_2.py
@@ -16,29 +16,37 @@ m_w = rho_w*pi*(d**3)*(4./3./8.)
 Cd = 0.47 #particula redonda
 #Euler en en x0
 dt = 1e-6*_s  #paso de tiempo
-tmax = 1 #tiempo maximo de simulacion
+tmax = 2#tiempo maximo de simulacion
 ti = 0. #tiempo actual 
 W = array([0, -m*g])
 #perfil logaritmico de velocidad en x
 k = 0.41
-def vx(y0,yi):
-	return -log(30*y0)/k
+
+def vx(y0,yi,v0):
+	if y0 < 1:
+		v0 = -log(30*y0)/k
+		return 2.8*(yi - y0) + v0
+	else:
+		return v0*log(30*y0)/k
 #Condiciones Iniciales
 n_particulas = 5 #Numero de particulas
-vf = array([5*_m*_s,0])
+a_rio = 11*_mm
+vf = array([1*_mm*_s,0])
 #historial de las particulas
 x_particulas = []
 v_particulas = [] 
 x_p = []
 y_p = []
 for i in range(n_particulas): 
+	valor = a_rio/n_particulas
 	x_p.append([])
 	y_p.append([])
-	v_x = 0
-	v_y = 0
-	p_x = (d)*random.random() + d
-	p_y = (d)*random.random() + d
+	v_x = 0 #random.random()*random.randint(-2,2)
+	v_y = 0 #random.random()*random.randint(-2,2)
+	p_x = 0.
+	p_y = (valor*(i+1)) + d 
 	xi = array([p_x,p_y])
+	print xi
 	vi = array([v_x,v_y])
 	x_particulas.append(xi)
 	v_particulas.append(vi)
@@ -47,27 +55,43 @@ xim1 = []
 vim1 = []
 while ti < tmax:
 	for i in range(n_particulas):
+		#Evaluando flujo del rio
 		if dt == 0:
-			u = vx(x_particulas[i][1], x_particulas[i][1])	
+			u = vx(x_particulas[i][1],x_particulas[i][1],vf[0])	
 			vf[0] = u
 		else:
-			u = vx(x_particulas[i - 1][1], x_particulas[i][1])	
+			u = vx(x_particulas[i - 1][1], x_particulas[i][1],vf[0])	
 			vf[0] = u
+		#Condicion de choque entre particulas
+		for t in range(n_particulas):
+			if i == t:
+				continue
+			else:
+				dif = sqrt((x_particulas[i][0] - x_particulas[t][0])**2 + (x_particulas[i][1] - x_particulas[t][1])**2)
+				if dif <= d :
 
-
+					v1_x = v_particulas[i][0]
+					v1_y = v_particulas[i][1]
+					v2_x = v_particulas[t][0]
+					v2_y = v_particulas[t][1]
+					v3_x = ((v1_x + v2_x) + abs(v1_x - v2_x))/2
+					v4_x = v1_x + v2_x - v3_x
+					v3_y = ((v1_y + v2_y) + abs(v1_y - v2_y))/2
+					v4_y = v1_y + v2_y - v3_y
+					v_particulas[i] = array([v3_x, v3_y])
+					v_particulas[t] = array([v4_x, v4_y])
+		#Condicion de suelo	
 		if x_particulas[i][1] <= d/2:
 			v_particulas[i][1] = -v_particulas[i][1]
 
 		#evaluar vrel
-		#print vf, v_particulas[i]
 		vrel = vf - v_particulas[i]
 		norm_vrel = norm(vrel)
 
 		#evaluar fuerzas sobre la particula
 		Fd = 0.5*Cd*norm_vrel*vrel #Drag force
-
 		Fb = array([0.,m_w*g]) #Bouyancy force
-		#print Fd
+
 		Fi = W + Fd + Fb
 		#evaluar aceleracion
 		ai = Fi / m
@@ -77,7 +101,6 @@ while ti < tmax:
 		#siguiente paso
 		x_p.append(x_particulas[i][0])
 		y_p.append(x_particulas[i][1])
-
 		ti += dt
 		x_p[i].append(x_particulas[i][0])
 		y_p[i].append(x_particulas[i][1])
