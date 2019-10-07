dde88b52c9ba782fb3cab90f90006710649ad7df
resnakk <masanchez3@miuandes.cl>
2019-10-07 01:36:20 -0300

    wea

--- README.md
+++ README.md
@@ -1,22 +1,19 @@
-# MCOC-proyect-2
-Integrantes:
-=============
+#README Mauricio Sánchez
+
+El presente trabajo busca modelar mediante un método lagrangiano el transporte de sedimentos en canales de agua de flujo continuo, no turbulento, principalmente de arenas finas y gravas. Sumado a lo anterior, el segundo foco de este proyecto, es estudiar como influye la decisión del algoritmo a implementar, y como varía su rendimiento a medida que se le exige más al modelo.
+
+Para la realización de este modelo se consideraron varios supuestos de tal manera que fuera valido: 
 ```
-Tomás García
-Javier Marín
-Mauricio Sánchez
+	1.- Se consideró que todas las particulas son identicas, y tienen un diametro de 2 mm, puesto que este diametro diferencia a las arenas finas de las gruesas.
+	2.- El modelo esta pensado para particulas esféricas perfectas, sin dar a alugar a particulas no uniformes. 
+	3.- Todas las particulas son identicas, pero inician sus trayectorias con posiciones y velocidades aleatorias
+	4.- Se asume que el suelo es liso, y sin coeficiente de roce. 
+	5.- El flujo del canal se rige por un perfil logaritmico basado en la ley de Von Kárnmán-Prandtl.
 ```
+Como este modelo busca predecir la trayectoria de varias particulas al mismo tiempo, se requiere de métodos de integración que retornen el comportamiento de estas, en base a las condiciones mencionadas anteriormente, los cuales se ven limitados por la cantidad de particulas, y por el tiempo en que se estudia su monvimiento.
 
-Introducción.
-=============
-
-En este proyecto se busca desarrollar un programa que permita mostrar el movimiento de una partícula a través de un flujo continuo.
-
-La anterior es una tarea de gran importancia y dificultad. Esto debido a que, de poder simular la forma en que se va a comportar una partícula en un momento determinado, se pueden hacer predicciones de su comportamiento a futuro.
-
-Actualmente, el desarrollo de este programa está siendo estudiado por ingenieros, que buscan poder entender el comportamiento que poseen ríos y, de esta forma, construir de manera que la estructura  aguante de la mejor manera posible las interacciones con el río.
-
-
+Primero se utilizó el método de euler, el cual se ve extremadamente limitado por el tiempo de estudio, más que por el número de 
+Para hacer esto se utilizó el método de euler para realizar la integración; este método si bien cumple su objetivo, se ve muy limitado cuando se aumenta mucho el numero de particulas
 Planteamiento.
 ==============
 
--- odeint.py
+++ odeint.py
@@ -17,8 +17,8 @@ m_w = rho_w*pi*(d**3)*(4./3./8.)
 Cd = 0.47 #drag coefficient
 Cvm = 0.5 #virtual mass coefficient
 #Euler en en x0
-dt = 1e-6*_s  #paso de tiempo
-tmax = 3#tiempo maximo de simulacion
+dt = 1e-5*_s  #paso de tiempo
+tmax = 2.5#tiempo maximo de simulacion
 t = arange(0, tmax, dt)
 W = array([0, -m*g])
 #perfil logaritmico de velocidad en x
@@ -26,19 +26,23 @@ k = 0.41
 #Condiciones Iniciales
 n_particulas = 5 #Numero de particulas
 a_rio = 11*_mm
-uf = array([250*_mm*_s,0])
+uf = array([500*_mm/_s,0])
 #Funciones
 def ux(y0,yi,u0):
 	if y0 < 1:
 		return 2.8*(yi - y0) + u0
 	else:
 		return log(30*yi)/k
+		print "hola"
 def movimiento(vector, t):
 	ret = []
 	i = 0
 	while i < len(vector):
 		pos_i = array([vector[i],vector[i + 1]])
 		vel_i = array([vector[i + 2],vector[i + 3]])
+		#print pos_i
+		#uf = -log(30*pos_i[1])/k
+		#print uf 
 		#condicion de choque
 		j = 0
 		while j < len(vector):
@@ -49,26 +53,26 @@ def movimiento(vector, t):
 				vel_j = array([vector[j + 2],vector[j + 3]])
 				dif = sqrt((pos_i[0] - pos_j[0])**2 + (pos_i[1] - pos_j[1])**2)
 				if dif <= d:
-					u1_x = pos_i[0]
-					u1_y = pos_j[1]
-					u2_x = pos_i[0]
-					u2_y = pos_j[1]
+					u1_x = vel_i[0]
+					u1_y = vel_i[1]
+					u2_x = vel_j[0]
+					u2_y = vel_j[1]
 					u3_x = ((u1_x + u2_x) + abs(u1_x - u2_x))/2
 					u4_x = u1_x + u2_x - u3_x
 					u3_y = ((u1_y + u2_y) + abs(u1_y - u2_y))/2
 					u4_y = u1_y + u2_y - u3_y
 					vel_i = array([u3_x,u3_y])
 					vel_j = array([u4_x,u4_y])
-					vector[j + 2] = u4_x
-					vector[j + 3] = u4_y
-					
-				 
+					#vector[j + 2] = u4_x
+					#vector[j + 3] = u4_y
+					j += 4
 			j += 4
 		#condicion de suelo
 		if pos_i[1] <= d/2:
 			vel_i[1] = -vel_i[1]
 		#Velocidad relativa
-		urel = uf - vel_i
+		urel = uf - vel_i 
+		
 		norm_urel = norm(urel)
 		#Fuerzas sobre la particula
 		Fd = 0.5*Cd*norm_urel*urel #Drag force
@@ -76,114 +80,57 @@ def movimiento(vector, t):
 		Fvm = Cvm*(uf)/dt #Virtual mass force Fvmc
 		Fi = W + Fd + Fb + Fvm
 		#retornos
+		acc = Fi/m
 		ret.append(pos_i[0])
 		ret.append(pos_i[1])
 		ret.append(vel_i[0])
 		ret.append(vel_i[1])
 		i += 4 
+
 	return ret 
 #Prediccion
 vector_inicial = []
 for i in range(n_particulas): 
 	valor = a_rio/n_particulas
-	u_x = random.random()*random.randint(0,5)
-	u_y = random.random()*random.randint(-2,10)
-	p_x = 0. #random.random()*0.004
+	u_x = random.random()*random.randint(1,5)
+	u_y = random.random()*random.randint(1,5)
+	p_x = random.random()*0.004
 	p_y = (valor*(i+1)) + d 
 	vector_inicial.append(p_x)
 	vector_inicial.append(p_y)
 	vector_inicial.append(u_x)
 	vector_inicial.append(u_y)
-print (len(vector_inicial))
 #Integracion con odeint	
 
+#exit()
 v_final = odeint(movimiento,vector_inicial, t)
 
-i = 0
-while i < range(len(v_final)):
-	plot(v_final[i], v_final[i + 1])
-	print v_final[i]
-	print len(v_final)
-	break
-show()
-
-
-
-
-"""
-pos_xy = []
-k = 0
-while k < len(v_final)/4:
-	if k%4 == 0:
-		pos_xy.append([])
-	pos = v_final[k]
-"""
-
-
-'''
-#Iteracion en el tiempo	
-xim1 = []
-uim1 = []
-while ti < tmax:
-	for i in range(n_particulas):
-		#Evaluando flujo del rio
-		u1 = array([vx(x_particulas[i - 1][1],x_particulas[i][1],uf[0]), 0])
-		u0 = uf	
-		uf[0] = u1[0]
-
-		#Condicion de choque entre particulas
-		for t in range(n_particulas):
-			if i == t:
-				continue
-			else:
-				dif = sqrt((x_particulas[i][0] - x_particulas[t][0])**2 + (x_particulas[i][1] - x_particulas[t][1])**2)
-				if dif <= d :
-
-					u1_x = u_particulas[i][0]
-					u1_y = u_particulas[i][1]
-					u2_x = u_particulas[t][0]
-					u2_y = u_particulas[t][1]
-					u3_x = ((u1_x + u2_x) + abs(u1_x - u2_x))/2
-					u4_x = u1_x + u2_x - u3_x
-					u3_y = ((u1_y + u2_y) + abs(u1_y - u2_y))/2
-					u4_y = u1_y + u2_y - u3_y
-					u_particulas[i] = array([u3_x, u3_y])
-					u_particulas[t] = array([u4_x, u4_y])
-		#Condicion de suelo	
-		if x_particulas[i][1] <= d/2:
-			u_particulas[i][1] = -u_particulas[i][1]
-
-		#evaluar vrel
-		urel = uf - u_particulas[i]
-		norm_urel = norm(urel)
-
-		#evaluar fuerzas sobre la particula
-		Fd = 0.5*Cd*norm_urel*urel #Drag force
-		Fb = array([0.,m_w*g]) #Bouyancy force
-		Fvm = Cvm*(u1 - u0)/dt #Virtual mass force Fvmc
-		Fi = W + Fd + Fb + Fvm
-		#evaluar aceleracion
-		ai = Fi / m
-		#integrar
-		xim1 = x_particulas[i] + u_particulas[i]*dt + ai*(dt**2)/2
-		uim1 = u_particulas[i] + ai*dt
-		#siguiente paso
-		x_p.append(x_particulas[i][0])
-		y_p.append(x_particulas[i][1])
-		ti += dt
-		x_p[i].append(x_particulas[i][0])
-		y_p[i].append(x_particulas[i][1])
-		x_particulas[i] = xim1
-		u_particulas[i] = uim1
-#Ploteo de las trayectorias
-xlabel("X")
-ylabel("Y")
-title("Trayectoria de particula")
+print v_final
+print len(v_final)
+x =[]
+y = []
 for i in range(n_particulas):
-	plot(x_p[i], y_p[i])
+	x.append([])
+	y.append([])
+
+
+i = 0
+while i < len(v_final) - 1:
+	cont = 0
+	j = 0
+	while j < len(v_final[i]) - 1:
+		if j%4 == 0:
+			x[cont].append(v_final[i][j])
+		if j%4 == 1:
+			y[cont].append(v_final[i][j])
+			cont += 1
+		j += 1
+	i += 1
+
+#exit()
+k = 0
+while k < len(x):
+	plot(x[k], y[k])
+	#print x[k]
+	k += 1
 show()
-#savefig("figura.png")
-'''
-
-
-
