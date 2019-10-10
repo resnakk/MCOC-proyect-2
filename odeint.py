e6cd7b8c1d43b3b26b1331376804de1729319ead
resnakk <masanchez3@miuandes.cl>
2019-10-10 03:13:26 -0300

    fuerzas añadidas

--- odeint.py
+++ odeint.py
@@ -1,14 +1,19 @@
 from matplotlib.pylab import *
 from scipy.integrate import odeint
 import random 
-#unidades SI
+#=================================================================unidades SI=================================================================
 _m = 1.
 _mm = _m*1e-3
 _gr = 1e-3
 _s = 1.
 _kg = 1.
-#Datos
+#=================================================================Condiciones Iniciales=================================================================
+n_particulas = 3 #Numero de particulas
+a_rio = 11*_mm
+uf = array([10*_mm/_s,0])
+#=================================================================Datos=================================================================
 d = 2*_mm
+A = pi*(d/2)**2
 g = 9.81*_m/_s**2
 rho = 2700*_kg/(_m**3)
 rho_w = 1000*_kg/(_m**3)
@@ -16,31 +21,31 @@ m = rho*pi*(d**3)*(4./3./8.)
 m_w = rho_w*pi*(d**3)*(4./3./8.)
 Cd = 0.47 #drag coefficient
 Cvm = 0.5 #virtual mass coefficient
+Cl = 0.2 #Lift force coefficient
+k_log = 0.41 #K para perfil logaritmico
+k_resorte = 1000*0.5*Cd*rho_w*A*norm(uf[0])/(1*_mm) #K para simular el choque 
 #Euler en en x0
-dt = 1e-5*_s  #paso de tiempo
-tmax = 1#tiempo maximo de simulacion
+dt = 1e-4*_s  #paso de tiempo
+tmax = 5#tiempo maximo de simulacion
 t = arange(0, tmax, dt)
 W = array([0, -m*g])
 #perfil logaritmico de velocidad en x
-k = 0.41
-#Condiciones Iniciales
-n_particulas = 5 #Numero de particulas
-a_rio = 11*_mm
-uf = array([5,0])
-#Funciones
+#=================================================================Funciones=================================================================
 def ux(y0,yi,u0):
 	if y0 < 1:
 		return 2.8*(yi - y0) + u0
 	else:
-		return log(30*yi)/k
+		return log(30*yi)/k_log
 def movimiento(vector, t):
-	ret = []
+	ret  = []
+	F_Choque = []
+	for i in range(len(vector)):
+		F_Choque.append(array([0,0]))
 	i = 0
 	while i < len(vector):
 		pos_i = array([vector[i],vector[i + 1]])
 		vel_i = array([vector[i + 2],vector[i + 3]]) 
-		#condicion de choque
-		F_Choque = [0,0]
+		#=================================================================condicion de choque=================================================================
 		j = 0
 		while j < len(vector):
 			if i == j:
@@ -49,53 +54,39 @@ def movimiento(vector, t):
 				pos_j = array([vector[j],vector[j + 1]])
 				vel_j = array([vector[j + 2],vector[j + 3]])
 				dif = sqrt((pos_i[0] - pos_j[0])**2 + (pos_i[1] - pos_j[1])**2)
-				rij = pos_j - pos_i
 				#Choque con Ley de hook
-				F_Choque = k*dif*rij/norm(rij)
-
-
-				'''
-				Choque con Conservacion de momentum y energia
-				if dif <= d:
-					u1_x = vel_i[0]
-					u1_y = vel_i[1]
-					u2_x = vel_j[0]
-					u2_y = vel_j[1]
-					u3_x = ((u1_x + u2_x) + abs(u1_x - u2_x))/2
-					u4_x = u1_x + u2_x - u3_x
-					u3_y = ((u1_y + u2_y) + abs(u1_y - u2_y))/2
-					u4_y = u1_y + u2_y - u3_y
-					vel_i = array([u3_x,u3_y])
-					vel_j = array([u4_x,u4_y])
-					vector[j + 2] = u4_x
-					vector[j + 3] = u4_y
-					j += 4
-				'''
+				if dif < d:
+					rij = pos_j - pos_i 
+					F_Choque[i/4] = k_resorte*dif*rij/norm(rij)
+					F_Choque[j/4] = -k_resorte*dif*rij/norm(rij)
 			j += 4
-		#condicion de suelo
+		#=================================================================condicion de suelo=================================================================
+		F_rebote = array([0,0])
 		if pos_i[1] <= d/2:
-			vel_i[1] = -vel_i[1]
-		#Velocidad relativa
+			rji = array([0, pos_i[1] - d/2])
+			F_rebote = -k_resorte*rji	 
+		#=================================================================Velocidad relativa=================================================================
 		urel = uf - vel_i 
 		norm_urel = norm(urel)
-		
-		#Fuerzas sobre la particula
+		#=================================================================Fuerzas sobre la particula=================================================================
 		Fd = 0.5*Cd*norm_urel*urel #Drag force
 		Fb = array([0.,m_w*g]) #Bouyancy force
-		#Fvm = Cvm*(uf)/dt #Virtual mass force Fvmc
-		Fi = W + Fd + Fb #+ Fvm
-			
-		#retornos
+		#Lift force
+		R = (rho/rho_w - 1)
+		alpha = (1 + R + Cvm)**-1 
+		Fl = array([0, (3/4)*alpha*Cl*(norm(0.9*vel_i)**2 - norm(1.1*vel_i)**2)])
+		#Virtual mass force
+		Fvm = array([-alpha*Cvm*vel_i[1]*uf[0]/(Cvm*pos_i[1]),0])
+		Fi = W + Fd + Fb + F_Choque[i/4] + F_rebote + Fl + Fvm
+		#=================================================================retornos=================================================================
 		acc = Fi/m
 		ret.append(vel_i[0])
 		ret.append(vel_i[1])
 		ret.append(acc[0])
 		ret.append(acc[1])
 		i += 4 
-
-	 
 	return ret
-#Prediccion
+#=================================================================Prediccion=================================================================
 vector_inicial = []
 for i in range(n_particulas): 
 	valor = a_rio/n_particulas
@@ -107,14 +98,10 @@ for i in range(n_particulas):
 	vector_inicial.append(p_y)
 	vector_inicial.append(u_x)
 	vector_inicial.append(u_y)
-#Integracion con odeint	
+#=================================================================Integracion con odeint=================================================================	
 v_final = odeint(movimiento,vector_inicial, t)
-
+print(v_final.shape)
 x = v_final[:,0::4]
 y = v_final[:,1::4]
-
-#plot(x,y,".-")
-
-plot(t,x[:,0])
-
+plot(x,y)
 show()
